2b5fc8cc91e87654910712722961b133c9fd91db
Isuru <isurumbandara@gmail.com>
2024-03-29 16:26:26 +0530

    commenting

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const App = () => {
 
 export default App;
 
-
+    //App.js
 // import React from 'react';
 // import { createStackNavigator } from '@react-navigation/stack';
 // import { NavigationContainer } from '@react-navigation/native';

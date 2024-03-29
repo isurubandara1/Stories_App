9f5a76fe4459aadcd51d3c2490dbd063860ed4cb
Isuru <isurumbandara@gmail.com>
2024-03-29 16:25:48 +0530

    commenting

--- App.js
+++ App.js
@@ -11,3 +11,24 @@ const App = () => {
 };
 
 export default App;
+
+
+// import React from 'react';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { Login } from './Login';
+// import { Register } from './Register';
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Register"> 
+//        <Stack.Screen name="Login" component={Login} /> 
+//         <Stack.Screen name="Register" component={Register} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+

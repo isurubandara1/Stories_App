18f4c1416c68e577f2a3661202fad2924fbb6528
Isuru <isurumbandara@gmail.com>
2024-04-03 14:57:01 +0530

    background image add

--- ShortStory.js
+++ ShortStory.js
@@ -1,120 +1,55 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  ScrollView,
-  SafeAreaView,
-} from "react-native";
-import { Appbar } from "react-native-paper"; // Importing Appbar component from react-native-paper library
-import { useNavigation } from "@react-navigation/native"; // Importing useNavigation hook from @react-navigation/native
+import { View, Text, StyleSheet, ImageBackground, ScrollView, SafeAreaView } from "react-native";
+import { Appbar } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
 export const ShortStory = () => {
-  const navigation = useNavigation(); // Initializing navigation using useNavigation hook
+  const navigation = useNavigation();
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "lightblue" }}>
-      <Appbar.Header style={styles.appbar}>
-      <Appbar.BackAction onPress={() => navigation.goBack()} color="white"/>
-      <Appbar.Content
-        title="Short Stories       "
-        titleStyle={styles.title}
-      />
-        {/* <Appbar.Action icon="calendar" onPress={() => {}} /> */}
-        {/* <Appbar.Action icon="magnify" onPress={() => {}} /> */}
-      </Appbar.Header>
-      
-      <ScrollView>
-        <View><Image style={styles.firstImage} source={require('./assets/ShortStory.jpg')}/></View>
-        <View style={styles.TopicView}>
-          <Text style={styles.TopicText}>Dog and Cat</Text>
-        </View>
-        <View style={styles.ContentView}>
-          <Text style={styles.Content}>
-            We are celebrating Asian Pacific American Heritage Month with
-            Krystal and other members of our Theatre Troupe today, and learning
-            about the ukulele! A small guitar-like instrument, the ukulele is a
-            member of the lute family of instruments and was introduced to
-            Hawaii by Portuguese immigrants from Madeira. Join us as we learn
-            about one of our favorite instruments in this singalong!
-          </Text>
-        </View>
-        <View style={styles.TopicView}>
-          <Text style={styles.TopicText}>Dog and Cat</Text>
-        </View>
-        <View style={styles.ContentView}>
-          <Text style={styles.Content}>
-            We are celebrating Asian Pacific American Heritage Month with
-            Krystal and other members of our Theatre Troupe today, and learning
-            about the ukulele! A small guitar-like instrument, the ukulele is a
-            member of the lute family of instruments and was introduced to
-            Hawaii by Portuguese immigrants from Madeira. Join us as we learn
-            about one of our favorite instruments in this singalong!
-          </Text>
-        </View>
-        <View style={styles.TopicView}>
-          <Text style={styles.TopicText}>Dog and Cat</Text>
-        </View>
-        <View style={styles.ContentView}>
-          <Text style={styles.Content}>
-            We are celebrating Asian Pacific American Heritage Month with
-            Krystal and other members of our Theatre Troupe today, and learning
-            about the ukulele! A small guitar-like instrument, the ukulele is a
-            member of the lute family of instruments and was introduced to
-            Hawaii by Portuguese immigrants from Madeira. Join us as we learn
-            about one of our favorite instruments in this singalong!
-          </Text>
-        </View>
-        <View style={styles.TopicView}>
-          <Text style={styles.TopicText}>Dog and Cat</Text>
-        </View>
-        <View style={styles.ContentView}>
-          <Text style={styles.Content}>
-            We are celebrating Asian Pacific American Heritage Month with
-            Krystal and other members of our Theatre Troupe today, and learning
-            about the ukulele! A small guitar-like instrument, the ukulele is a
-            member of the lute family of instruments and was introduced to
-            Hawaii by Portuguese immigrants from Madeira. Join us as we learn
-            about one of our favorite instruments in this singalong!
-          </Text>
-        </View>
-        <View style={styles.TopicView}>
-          <Text style={styles.TopicText}>Dog and Cat</Text>
-        </View>
-        <View style={styles.ContentView}>
-          <Text style={styles.Content}>
-            We are celebrating Asian Pacific American Heritage Month with
-            Krystal and other members of our Theatre Troupe today, and learning
-            about the ukulele! A small guitar-like instrument, the ukulele is a
-            member of the lute family of instruments and was introduced to
-            Hawaii by Portuguese immigrants from Madeira. Join us as we learn
-            about one of our favorite instruments in this singalong!
-          </Text>
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={require('./assets/ShortStory.jpg')} style={styles.backgroundImage}>
+        <Appbar.Header style={styles.appbar}>
+          <Appbar.BackAction onPress={() => navigation.goBack()} color="white" />
+          <Appbar.Content title="Short Stories" titleStyle={styles.title} />
+        </Appbar.Header>
+
+        <ScrollView>
+          <View style={styles.TopicView}>
+            <Text style={styles.TopicText}>Dog and Cat</Text>
+          </View>
+          <View style={styles.ContentView}>
+            <Text style={styles.Content}>
+              We are celebrating Asian Pacific American Heritage Month with
+              Krystal and other members of our Theatre Troupe today, and learning
+              about the ukulele! A small guitar-like instrument, the ukulele is a
+              member of the lute family of instruments and was introduced to
+              Hawaii by Portuguese immigrants from Madeira. Join us as we learn
+              about one of our favorite instruments in this singalong!
+            </Text>
+          </View>
+          
+        </ScrollView>
+      </ImageBackground>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-    appbar: {
-        backgroundColor: 'yellowgreen',
-      },
-      title: {
-        fontSize: 28,
-        color: 'white',
-        fontWeight:"bold",
-        fontStyle:"italic",
-        textAlign:"center",
-      },
-      firstImage:{
-        width:"100%",
-        height:250,
-        resizeMode:"cover",
-        borderBottomLeftRadius:30,
-        borderBottomRightRadius:30,
-      },
-  
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  appbar: {
+    backgroundColor: 'yellowgreen',
+  },
+  title: {
+    fontSize: 28,
+    color: 'white',
+    fontWeight: "bold",
+    fontStyle: "italic",
+    textAlign: "center",
+  },
   TopicView: {
     marginTop: 20,
     marginBottom: 10,
@@ -138,3 +73,5 @@ const styles = StyleSheet.create({
     textAlign: "justify",
   },
 });
+
+export default ShortStory;
